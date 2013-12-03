ca433003f73d15c82e09ef647eecf08a1e42092d
Girish Chopra <girish@Girishs-Mac-mini.local>
2013-12-03 22:38:38 +0000

    Initial Commit

---
+++ MiHealthChoice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiHealthChoice
+//
+//  Created by Girish Chopra on 03/12/2013.
+//  Copyright (c) 2013 Intuiti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiHealthChoice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MiHealthChoice
+//
+//  Created by Girish Chopra on 03/12/2013.
+//  Copyright (c) 2013 Intuiti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
